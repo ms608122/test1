3000b2cbb1f4d722f214e71878efc51b0a9671d7
ms608122 <ms608122@ohio.edu>
2025-10-30 17:52:22 -0400

    ,

---
+++ cpp-unit-test/calculator.h
@@ -0,0 +1,10 @@
+#pragma once
+// calculator.h
+// -----------------------------
+// Header file declaring simple arithmetic functions.
+// These functions are implemented in calculator.cpp.
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
